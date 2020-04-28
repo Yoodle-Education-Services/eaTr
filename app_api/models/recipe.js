20341fd443b7827595eb3f05c3fcafc145780cb0
Dr-Wrong-Mo <dr.wrong.mo@gmail.com>
2020-04-28 18:04:23 -0500

    Commit root folder files

---
+++ app_api/models/recipe.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const ingredientsSchema = new mongoose.Schema({
+    ingredient: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        Number,
+        required: true
+    },
+    unitOfMeasure: String,
+    required: false
+});
+
+const recipeSchema = new mongoose.Schema({
+    recipeName: {
+        type: String,
+        required: true
+    },
+    ingredients: [ingredientsSchema],
+    instructionsSchema: {
+        type: String,
+        required: true
+    },
+    image: ({
+        img: { data: Buffer, contentType: String }
+    }),
+});
+
+const shoppingListSchema = new mongoose.Schema({
+    ingredients: [ingredientsSchema],
+}),
+
+const chefSchema = new mongoose.Schema({
+    chefName: {
+    type: String,
+    required: true
+    },
+    recipes: [recipeSchema],
+    shoppingList: [shoppingListSchema]
+});
+
+mongoose.model('recipe', recipeSchema);
+mongoose.model('shoppingList', shoppingListSchema);
+mongoose.model('chef', chefSchemaSchema);
