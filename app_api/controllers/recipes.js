93251c48838ea37a59781864f4a3c81a4a6b8ef4
Delphine  Dara <delphine.dara@gmail.com>
2020-05-04 15:12:01 -0500

    modified recipeCreate

--- app_api/controllers/recipes.js
+++ app_api/controllers/recipes.js
@@ -1,35 +1,63 @@
 const mongoose = require('mongoose');
 const Rec = mongoose.model('Recipe');
 
+
 // recipesCreate controller
+//note sure of recipesCreate ???
 const recipesCreate=(req, res) =>{
   Rec.create({
     recipeName: req.body.recipeName,
-    recipeImage:req.body.recipeImage,
-    ingredients:{
-      ingredient1:req.body.ingredient1,
-      ingredient2:req.body.ingredient2,
-      ingredient3:req.body.ingredient3,
-      ingredient4:req.body.ingredient4,
-      ingredient5:req.body.ingredient5
-     //loop function is better
-    },(err, recipe) => {
-      if (err) {
-        res
-          .status(400)
-          .json(err);
-      } else {
-        res
-          .status(201)
-          .json(recipe);
+    recipeImage:req.body.recipeImage, 
+    recipeInstructions:req.body.instructions, //Does this also need a function?
+    //create a function - ingredient function calls ingredient name, measurement
+    // This is what visual code suggested --------
+    recipeIngredients:[{
+      //ingredient name - string - required
+      ingredientName: req.body.ingredientName1,
+      get ingredientName() {
+        return this.ingredientName;
+      },
+      set ingredientName(value) {
+        this.ingredientName = value;
+      },
+      //ingredient quantity - number- required
+      ingredientQuantity: req.body.ingredientQuantity1,
+      get ingredientQuantity(){
+        get ingredientQuantity() {
+          return this.ingredientQuantity;
+        },
+        set ingredientQuantity(value) {
+          this.ingredientQuantity = value;
+        },
+
+     //ingredient unitofMeasure - string- not required ?? Do we want to require this?
+      ingredientUnitofMeasure: req.body.ingredientUnitofMeasure1,
+      get ingredientUnitofMeasure(){
+        get ingredientUnitofMeasure) {
+          return this.ingredientUnitofMeasure;
+        },
+        set ingredientUnitofMeasure(value) {
+          this.ingredientUnitofMeasure = value;
+        },
+
       }
-    };
-   
-  });
-}
-
-
-
+    ]
+  } 
+  recipe.save((err,rec)=>{
+    if (err){
+      res
+      .status(404)
+      .json(err);
+    } else{
+      res
+      .status(200)
+      .json(rec);
+    }
+  })
+  
+  };
+  );
+};
 
 // recipesReadOne controller
 const recipesReadOne = (req, res) => {
@@ -54,12 +82,31 @@ const recipesReadOne = (req, res) => {
 };
 
 
-// recipesUpdateOne
+// recipesUpdateOne Controller
 const recipesUpdateOne = (req, res) => {
-  
-};
-
-// recipesDeleteOne
+  if (!req.params.recipeid) {
+    return res
+      .status(404)
+      .json({
+        "message": "Not found, recipeid is required"
+      });
+  }
+  Rec
+    .findById(req.params.recipeid)
+    .exec((err, recipe) => {
+      if (!recipe) {
+        return res
+          .status(404)
+          .json({
+            "message": "recipeid not found"
+          });
+      } else if (err) {
+        return res
+          .status(400)
+          .json(err);
+      }
+      
+// recipesDeleteOne 
 const recipesDeleteOne = (req, res) =>  {
   const {recipeid} = req.params;
   if (recipeid) {
